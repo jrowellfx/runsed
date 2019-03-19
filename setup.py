565493d0bcec5410dc489b782911cf36438350dd
James Philip Rowell <james.rowell@yahoo.com>
2019-03-19 10:32:43 -0500

    Added scripts to install runsed on system

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+from distutils.core import setup
+import os
+import shutil
+
+setup(name='runsed',
+      version='1.000',
+      description='Wrapper for safely running sed on files.',
+      long_description='',
+      author='James Philip Rowell',
+      author_email='james@alpha-eleven.com',
+      url='http://www.alpha-eleven.com/',
+      py_modules=[],
+      scripts=['runsed'],
+      license = "BSD 3-Clause license",
+     )
